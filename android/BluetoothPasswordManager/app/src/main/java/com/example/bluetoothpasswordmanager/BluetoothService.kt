6897e47a513b1405e7b677af281dcc821834fab5
starcev_misha <starcev_misha@mail.ru>
2020-03-15 16:27:19 +0500

    add AES cypher

--- android/BluetoothPasswordManager/app/src/main/java/com/example/bluetoothpasswordmanager/BluetoothService.kt
+++ android/BluetoothPasswordManager/app/src/main/java/com/example/bluetoothpasswordmanager/BluetoothService.kt
@@ -4,19 +4,25 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
 import android.util.Log
 import java.io.IOException
-import java.math.BigInteger
 
 
 import java.util.*
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
+private val AES_SECRET_KEY = byteArrayOf(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F)
 
 private const val TAG = "BluetoothService"
 private val uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
-private val p : Long = 2147483647
-private val g : Long = 16807
 
 
+private fun encrypt(raw: ByteArray, clear: ByteArray): ByteArray {
+    val skeySpec = SecretKeySpec(raw, "AES")
+    val cipher = Cipher.getInstance("AES")
+    cipher.init(Cipher.ENCRYPT_MODE, skeySpec)
+    return cipher.doFinal(clear)
+}
 
 class BluetoothService {
     private var secretKey : Long = 0
@@ -56,30 +62,31 @@ class BluetoothService {
                 return false
             }
 
-            if (isSecure) {
-                val a = (1..p).random() // Diffie-Hellman privateKey
-                val A = BigInteger.valueOf(g).modPow(BigInteger.valueOf(a), BigInteger.valueOf(p)).toLong()
-                val message = "dhs p=$p g=$g A=$A\n"
-                bTSocket!!.outputStream!!.write(message.toByteArray())
-                sleep(100)
-                if (bTSocket!!.inputStream!!.available() > 0){
-                    val available = bTSocket!!.inputStream!!.available()
-                    val bytes = ByteArray(available)
-                    Log.i("server", "Reading")
-                    bTSocket!!.inputStream!!.read(bytes, 0, available)
-                    val text = String(bytes)
-                    Log.i("server", "Message received")
-                    Log.i("server", "B= $text")
+//            if (isSecure) {
+////                val a = (1..p).random() // Diffie-Hellman privateKey
+////                val A = BigInteger.valueOf(g).modPow(BigInteger.valueOf(a), BigInteger.valueOf(p)).toLong()
+////                val message = "dhs p=$p g=$g A=$A\n"
+////                bTSocket!!.outputStream!!.write(message.toByteArray())
+////                sleep(100)
+////                if (bTSocket!!.inputStream!!.available() > 0){
+////                    val available = bTSocket!!.inputStream!!.available()
+////                    val bytes = ByteArray(available)
+////                    Log.i("server", "Reading")
+////                    bTSocket!!.inputStream!!.read(bytes, 0, available)
+////                    val text = String(bytes)
+////                    Log.i("server", "Message received")
+////                    Log.i("server", "B= $text")
+////
+////                    val B = text.toLongOrNull()
+////                    if (B != null) {
+////                        secretKey = BigInteger.valueOf(B).modPow(BigInteger.valueOf(a), BigInteger.valueOf(p)).toLong()
+////                        Log.i("server", "secretKey= $secretKey")
+////                    }
+////                }
+////
+////
+////            }
 
-                    val B = text.toLongOrNull()
-                    if (B != null) {
-                        secretKey = BigInteger.valueOf(B).modPow(BigInteger.valueOf(a), BigInteger.valueOf(p)).toLong()
-                        Log.i("server", "secretKey= $secretKey")
-                    }
-                }
-
-
-            }
 
             return true
         }
@@ -91,10 +98,9 @@ class BluetoothService {
 
     fun write(bytes: ByteArray) {
         try {
-            if (secretKey > 0) {
-                //code bytes
-            }
-            bTSocket?.outputStream?.write(bytes)
+            val encrypted = encrypt(AES_SECRET_KEY, bytes)
+            bTSocket?.outputStream?.write(encrypted)
+//            bTSocket?.outputStream?.write(bytes)
         } catch (e: IOException) {
             Log.e(TAG, "Error occurred when sending data", e)
         }
